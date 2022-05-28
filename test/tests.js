e8316b706d6eb10c81e661e8595e5386a5147de3
Jon Koops <jonkoops@gmail.com>
2022-05-28 17:12:47 +0200

    Use more modern JavaScript syntax (#88)

--- test/tests.js
+++ test/tests.js
@@ -1,38 +1,38 @@
 import assert from 'assert';
 import urlJoin from '../lib/url-join.js';
 
-describe('url join', function () {
-  it('should work for simple case', function () {
+describe('url join', () => {
+  it('should work for simple case', () => {
     urlJoin('http://www.google.com/', 'foo/bar', '?test=123')
       .should.eql('http://www.google.com/foo/bar?test=123');
   });
 
-  it('should work for simple case with new syntax', function () {
+  it('should work for simple case with new syntax', () => {
     urlJoin(['http://www.google.com/', 'foo/bar', '?test=123'])
       .should.eql('http://www.google.com/foo/bar?test=123');
   });
 
-  it('should work for hashbang urls', function () {
+  it('should work for hashbang urls', () => {
     urlJoin(['http://www.google.com', '#!', 'foo/bar', '?test=123'])
       .should.eql('http://www.google.com/#!/foo/bar?test=123');
   });
 
-  it('should be able to join protocol', function () {
+  it('should be able to join protocol', () => {
     urlJoin('http:', 'www.google.com/', 'foo/bar', '?test=123')
       .should.eql('http://www.google.com/foo/bar?test=123');
   });
 
-  it('should be able to join protocol with slashes', function () {
+  it('should be able to join protocol with slashes', () => {
     urlJoin('http://', 'www.google.com/', 'foo/bar', '?test=123')
       .should.eql('http://www.google.com/foo/bar?test=123');
   });
 
-  it('should remove extra slashes', function () {
+  it('should remove extra slashes', () => {
     urlJoin('http:', 'www.google.com///', 'foo/bar', '?test=123')
       .should.eql('http://www.google.com/foo/bar?test=123');
   });
 
-  it('should not remove extra slashes in an encoded URL', function () {
+  it('should not remove extra slashes in an encoded URL', () => {
     urlJoin('http:', 'www.google.com///', 'foo/bar', '?url=http%3A//Ftest.com')
       .should.eql('http://www.google.com/foo/bar?url=http%3A//Ftest.com');
 
@@ -41,17 +41,17 @@ describe('url join', function () {
       .should.not.eql('http://a.com/23d04b3//b/c.html');
   });
 
-  it('should support anchors in urls', function () {
+  it('should support anchors in urls', () => {
     urlJoin('http:', 'www.google.com///', 'foo/bar', '?test=123', '#faaaaa')
       .should.eql('http://www.google.com/foo/bar?test=123#faaaaa');
   });
 
-  it('should support protocol-relative urls', function () {
+  it('should support protocol-relative urls', () => {
     urlJoin('//www.google.com', 'foo/bar', '?test=123')
       .should.eql('//www.google.com/foo/bar?test=123')
   });
 
-  it('should support file protocol urls', function () {
+  it('should support file protocol urls', () => {
     urlJoin('file:/', 'android_asset', 'foo/bar')
       .should.eql('file://android_asset/foo/bar')
 
@@ -59,7 +59,7 @@ describe('url join', function () {
       .should.eql('file://android_asset/foo/bar')
   });
 
-  it('should support absolute file protocol urls', function () {
+  it('should support absolute file protocol urls', () => {
     urlJoin('file:', '///android_asset', 'foo/bar')
       .should.eql('file:///android_asset/foo/bar')
 
@@ -73,7 +73,7 @@ describe('url join', function () {
       .should.eql('file:///android_asset/foo/bar')
   });
 
-  it('should merge multiple query params properly', function () {
+  it('should merge multiple query params properly', () => {
     urlJoin('http:', 'www.google.com///', 'foo/bar', '?test=123', '?key=456')
       .should.eql('http://www.google.com/foo/bar?test=123&key=456');
 
@@ -84,22 +84,22 @@ describe('url join', function () {
       .should.eql('http://example.org/x?a=1&b=2&c=3&d=4');
   });
 
-  it('should merge slashes in paths correctly', function () {
+  it('should merge slashes in paths correctly', () => {
     urlJoin('http://example.org', 'a//', 'b//', 'A//', 'B//')
       .should.eql('http://example.org/a/b/A/B/');
   });
 
-  it('should merge colons in paths correctly', function () {
+  it('should merge colons in paths correctly', () => {
     urlJoin('http://example.org/', ':foo:', 'bar')
       .should.eql('http://example.org/:foo:/bar');
   });
 
-  it('should merge just a simple path without URL correctly', function() {
+  it('should merge just a simple path without URL correctly', () => {
     urlJoin('/', 'test')
       .should.eql('/test');
   });
 
-  it('should fail with segments that are not string', function() {
+  it('should fail with segments that are not string', () => {
     assert.throws(() => urlJoin(true),
                   /Url must be a string. Received true/);
     assert.throws(() => urlJoin('http://blabla.com/', 1),
@@ -112,12 +112,12 @@ describe('url join', function () {
                   /Url must be a string. Received \[object Object\]/);
   });
 
-  it('should merge a path with colon properly', function(){
+  it('should merge a path with colon properly', () => {
     urlJoin('/users/:userId', '/cars/:carId')
       .should.eql('/users/:userId/cars/:carId');
   });
 
-  it('should merge slashes in protocol correctly', function () {
+  it('should merge slashes in protocol correctly', () => {
     urlJoin('http://example.org', 'a')
       .should.eql('http://example.org/a');
     urlJoin('http:', '//example.org', 'a')
@@ -138,18 +138,18 @@ describe('url join', function () {
       .should.eql('file://example.org/a');
   });
 
-  it('should skip empty strings', function() {
+  it('should skip empty strings', () => {
     urlJoin('http://foobar.com', '', 'test')
       .should.eql('http://foobar.com/test');
     urlJoin('', 'http://foobar.com', '', 'test')
       .should.eql('http://foobar.com/test');
   });
 
-  it('should return an empty string if no arguments are supplied', function() {
+  it('should return an empty string if no arguments are supplied', () => {
     urlJoin().should.eql('');
   });
 
-  it('should not mutate the original reference', function() {
+  it('should not mutate the original reference', () => {
     const input = ['http:', 'www.google.com/', 'foo/bar', '?test=123'];
     const expected = Array.from(input);
 
