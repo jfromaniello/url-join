81b0d82e126df071b56c22d35276ac1a3d39605d
Jon Koops <jonkoops@gmail.com>
2023-11-11 19:12:37 +0100

    Use built-in Node.js test runner (#102)

--- test/tests.js
+++ test/tests.js
@@ -1,174 +1,269 @@
-import assert from 'assert';
+import assert from 'node:assert/strict';
+import test from 'node:test';
 import urlJoin from '../lib/url-join.js';
 
-describe('url join', () => {
-  it('should work for simple case', () => {
-    urlJoin('http://www.google.com/', 'foo/bar', '?test=123')
-      .should.eql('http://www.google.com/foo/bar?test=123');
-  });
+test('joins a simple URL', () => {
+  assert.equal(
+    urlJoin('http://www.google.com/', 'foo/bar', '?test=123'),
+    'http://www.google.com/foo/bar?test=123'
+  );
+});
 
-  it('should work for simple case with new syntax', () => {
-    urlJoin(['http://www.google.com/', 'foo/bar', '?test=123'])
-      .should.eql('http://www.google.com/foo/bar?test=123');
-  });
+test('joins a simple URL with new syntax', () => {
+  assert.equal(
+    urlJoin(['http://www.google.com/', 'foo/bar', '?test=123']),
+    'http://www.google.com/foo/bar?test=123'
+  );
+});
 
-  it('should work for hashbang urls', () => {
-    urlJoin(['http://www.google.com', '#!', 'foo/bar', '?test=123'])
-      .should.eql('http://www.google.com/#!/foo/bar?test=123');
-  });
+test('joins a hashbang', () => {
+  assert.equal(
+    urlJoin(['http://www.google.com', '#!', 'foo/bar', '?test=123']),
+    'http://www.google.com/#!/foo/bar?test=123'
+  );
+});
 
-  it('should be able to join protocol', () => {
-    urlJoin('http:', 'www.google.com/', 'foo/bar', '?test=123')
-      .should.eql('http://www.google.com/foo/bar?test=123');
-  });
+test('joins a protocol', () => {
+  assert.equal(
+    urlJoin('http:', 'www.google.com/', 'foo/bar', '?test=123'),
+    'http://www.google.com/foo/bar?test=123'
+  );
+});
 
-  it('should be able to join protocol with slashes', () => {
-    urlJoin('http://', 'www.google.com/', 'foo/bar', '?test=123')
-      .should.eql('http://www.google.com/foo/bar?test=123');
-  });
+test('joins a protocol with slashes', () => {
+  assert.equal(
+    urlJoin('http://', 'www.google.com/', 'foo/bar', '?test=123'),
+    'http://www.google.com/foo/bar?test=123'
+  );
+});
 
-  it('should remove extra slashes', () => {
-    urlJoin('http:', 'www.google.com///', 'foo/bar', '?test=123')
-      .should.eql('http://www.google.com/foo/bar?test=123');
-  });
+test('removes extra slashes', () => {
+  assert.equal(
+    urlJoin('http:', 'www.google.com///', 'foo/bar', '?test=123'),
+    'http://www.google.com/foo/bar?test=123'
+  );
+});
 
-  it('should not remove extra slashes in an encoded URL', () => {
-    urlJoin('http:', 'www.google.com///', 'foo/bar', '?url=http%3A//Ftest.com')
-      .should.eql('http://www.google.com/foo/bar?url=http%3A//Ftest.com');
+test('removes extra slashes in an encoded URL', () => {
+  assert.equal(
+    urlJoin('http:', 'www.google.com///', 'foo/bar', '?url=http%3A//Ftest.com'),
+    'http://www.google.com/foo/bar?url=http%3A//Ftest.com'
+  );
 
-    urlJoin('http://a.com/23d04b3/', '/b/c.html')
-      .should.eql('http://a.com/23d04b3/b/c.html')
-      .should.not.eql('http://a.com/23d04b3//b/c.html');
-  });
+  assert.equal(
+    urlJoin('http://a.com/23d04b3/', '/b/c.html'),
+    'http://a.com/23d04b3/b/c.html'
+  );
+});
 
-  it('should support anchors in urls', () => {
-    urlJoin('http:', 'www.google.com///', 'foo/bar', '?test=123', '#faaaaa')
-      .should.eql('http://www.google.com/foo/bar?test=123#faaaaa');
-  });
+test('joins anchors in URLs', () => {
+  assert.equal(
+    urlJoin('http:', 'www.google.com///', 'foo/bar', '?test=123', '#faaaaa'),
+    'http://www.google.com/foo/bar?test=123#faaaaa'
+  );
+});
 
-  it('should support protocol-relative urls', () => {
-    urlJoin('//www.google.com', 'foo/bar', '?test=123')
-      .should.eql('//www.google.com/foo/bar?test=123')
-  });
+test('joins protocol-relative URLs', () => {
+  assert.equal(
+    urlJoin('//www.google.com', 'foo/bar', '?test=123'),
+    '//www.google.com/foo/bar?test=123'
+  );
+});
 
-  it('should support file protocol urls', () => {
-    urlJoin('file:/', 'android_asset', 'foo/bar')
-      .should.eql('file://android_asset/foo/bar')
+test('joins file protocol URLs', () => {
+  assert.equal(
+    urlJoin('file:/', 'android_asset', 'foo/bar'),
+    'file://android_asset/foo/bar'
+  );
 
-    urlJoin('file:', '/android_asset', 'foo/bar')
-      .should.eql('file://android_asset/foo/bar')
-  });
+  assert.equal(
+    urlJoin('file:', '/android_asset', 'foo/bar'),
+    'file://android_asset/foo/bar'
+  );
+});
 
-  it('should support absolute file protocol urls', () => {
-    urlJoin('file:', '///android_asset', 'foo/bar')
-      .should.eql('file:///android_asset/foo/bar')
+test('joins absolute file protocol URLs', () => {
+  assert.equal(
+    urlJoin('file:', '///android_asset', 'foo/bar'),
+    'file:///android_asset/foo/bar'
+  );
 
-    urlJoin('file:///', 'android_asset', 'foo/bar')
-      .should.eql('file:///android_asset/foo/bar')
+  assert.equal(
+    urlJoin('file:///', 'android_asset', 'foo/bar'),
+    'file:///android_asset/foo/bar'
+  );
 
-    urlJoin('file:///', '//android_asset', 'foo/bar')
-      .should.eql('file:///android_asset/foo/bar')
+  assert.equal(
+    urlJoin('file:///', '//android_asset', 'foo/bar'),
+    'file:///android_asset/foo/bar'
+  );
 
-    urlJoin('file:///android_asset', 'foo/bar')
-      .should.eql('file:///android_asset/foo/bar')
-  });
+  assert.equal(
+    urlJoin('file:///android_asset', 'foo/bar'),
+    'file:///android_asset/foo/bar'
+  );
+});
 
-  it('should merge multiple query params properly', () => {
-    urlJoin('http:', 'www.google.com///', 'foo/bar', '?test=123', '?key=456')
-      .should.eql('http://www.google.com/foo/bar?test=123&key=456');
+test('joins multiple query params', () => {
+  assert.equal(
+    urlJoin('http:', 'www.google.com///', 'foo/bar', '?test=123', '?key=456'),
+    'http://www.google.com/foo/bar?test=123&key=456'
+  );
 
-    urlJoin('http:', 'www.google.com///', 'foo/bar', '?test=123', '?boom=value', '&key=456')
-      .should.eql('http://www.google.com/foo/bar?test=123&boom=value&key=456');
+  assert.equal(
+    urlJoin('http:', 'www.google.com///', 'foo/bar', '?test=123', '?boom=value', '&key=456'),
+    'http://www.google.com/foo/bar?test=123&boom=value&key=456'
+  );
 
-    urlJoin('http://example.org/x', '?a=1', '?b=2', '?c=3', '?d=4')
-      .should.eql('http://example.org/x?a=1&b=2&c=3&d=4');
+  assert.equal(
+    urlJoin('http://example.org/x', '?a=1', '?b=2', '?c=3', '?d=4'),
+    'http://example.org/x?a=1&b=2&c=3&d=4'
+  );
 
-    urlJoin('http:', 'www.google.com///', 'foo/bar', '&test=123', '&key=456')
-      .should.eql('http://www.google.com/foo/bar?test=123&key=456');
+  assert.equal(
+    urlJoin('http:', 'www.google.com///', 'foo/bar', '&test=123', '&key=456'),
+    'http://www.google.com/foo/bar?test=123&key=456'
+  );
 
-    urlJoin('http:', 'www.google.com///', 'foo/bar', '&test=123', '?key=456')
-      .should.eql('http://www.google.com/foo/bar?test=123&key=456');
-  });
+  assert.equal(
+    urlJoin('http:', 'www.google.com///', 'foo/bar', '&test=123', '?key=456'),
+    'http://www.google.com/foo/bar?test=123&key=456'
+  );
+});
 
-  it('filters out empty query parameters', () => {
-    urlJoin('http://google.com', '?')
-      .should.eql('http://google.com');
+test('filters out empty query parameters', () => {
+  assert.equal(
+    urlJoin('http://google.com', '?'),
+    'http://google.com'
+  );
 
-    urlJoin('http://google.com', '&')
-      .should.eql('http://google.com');
-  });
+  assert.equal(
+    urlJoin('http://google.com', '&'),
+    'http://google.com'
+  );
+});
 
-  it('should merge slashes in paths correctly', () => {
-    urlJoin('http://example.org', 'a//', 'b//', 'A//', 'B//')
-      .should.eql('http://example.org/a/b/A/B/');
-  });
+test('joins slashes in paths', () => {
+  assert.equal(
+    urlJoin('http://example.org', 'a//', 'b//', 'A//', 'B//'),
+    'http://example.org/a/b/A/B/'
+  );
+});
 
-  it('should merge colons in paths correctly', () => {
-    urlJoin('http://example.org/', ':foo:', 'bar')
-      .should.eql('http://example.org/:foo:/bar');
-  });
+test('joins colons in paths', () => {
+  assert.equal(
+    urlJoin('http://example.org/', ':foo:', 'bar'),
+    'http://example.org/:foo:/bar'
+  );
+});
 
-  it('should merge just a simple path without URL correctly', () => {
-    urlJoin('/', 'test')
-      .should.eql('/test');
-  });
+test('joins a simple path without a URL', () => {
+  assert.equal(
+    urlJoin('/', 'test'),
+    '/test'
+  );
+});
 
-  it('should fail with segments that are not string', () => {
-    assert.throws(() => urlJoin(true),
-                  /Url must be a string. Received true/);
-    assert.throws(() => urlJoin('http://blabla.com/', 1),
-                  /Url must be a string. Received 1/);
-    assert.throws(() => urlJoin('http://blabla.com/', undefined, 'test'),
-                  /Url must be a string. Received undefined/);
-    assert.throws(() => urlJoin('http://blabla.com/', null, 'test'),
-                  /Url must be a string. Received null/);
-    assert.throws(() => urlJoin('http://blabla.com/', { foo: 123 }, 'test'),
-                  /Url must be a string. Received \[object Object\]/);
-  });
+test('throws for segments that are not a string', () => {
+  assert.throws(
+    () => urlJoin(true),
+    /Url must be a string. Received true/
+  );
+  
+  assert.throws(
+    () => urlJoin('http://blabla.com/', 1),
+    /Url must be a string. Received 1/
+  );
+  
+  assert.throws(
+    () => urlJoin('http://blabla.com/', undefined, 'test'),
+    /Url must be a string. Received undefined/
+  );
 
-  it('should merge a path with colon properly', () => {
-    urlJoin('/users/:userId', '/cars/:carId')
-      .should.eql('/users/:userId/cars/:carId');
-  });
+  assert.throws(
+    () => urlJoin('http://blabla.com/', null, 'test'),
+    /Url must be a string. Received null/
+  );
+  
+  assert.throws(
+    () => urlJoin('http://blabla.com/', { foo: 123 }, 'test'),
+    /Url must be a string. Received \[object Object\]/
+  );
+});
 
-  it('should merge slashes in protocol correctly', () => {
-    urlJoin('http://example.org', 'a')
-      .should.eql('http://example.org/a');
-    urlJoin('http:', '//example.org', 'a')
-      .should.eql('http://example.org/a');
-    urlJoin('http:///example.org', 'a')
-      .should.eql('http://example.org/a');
-    urlJoin('file:///example.org', 'a')
-      .should.eql('file:///example.org/a');
+test('joins a path with a colon', () => {
+  assert.equal(
+    urlJoin('/users/:userId', '/cars/:carId'),
+    '/users/:userId/cars/:carId'
+  );
+});
 
-    urlJoin('file:example.org', 'a')
-      .should.eql('file://example.org/a');
+test('joins slashes in the protocol', () => {
+  assert.equal(
+    urlJoin('http://example.org', 'a'),
+    'http://example.org/a'
+  );
 
-    urlJoin('file:/', 'example.org', 'a')
-      .should.eql('file://example.org/a');
-    urlJoin('file:', '/example.org', 'a')
-      .should.eql('file://example.org/a');
-    urlJoin('file:', '//example.org', 'a')
-      .should.eql('file://example.org/a');
-  });
+  assert.equal(
+    urlJoin('http:', '//example.org', 'a'),
+    'http://example.org/a'
+  );
 
-  it('should skip empty strings', () => {
-    urlJoin('http://foobar.com', '', 'test')
-      .should.eql('http://foobar.com/test');
-    urlJoin('', 'http://foobar.com', '', 'test')
-      .should.eql('http://foobar.com/test');
-  });
+  assert.equal(
+    urlJoin('http:///example.org', 'a'),
+    'http://example.org/a'
+  );
 
-  it('should return an empty string if no arguments are supplied', () => {
-    urlJoin().should.eql('');
-  });
+  assert.equal(
+    urlJoin('file:///example.org', 'a'),
+    'file:///example.org/a'
+  );
 
-  it('should not mutate the original reference', () => {
-    const input = ['http:', 'www.google.com/', 'foo/bar', '?test=123'];
-    const expected = Array.from(input);
+  assert.equal(
+    urlJoin('file:example.org', 'a'),
+    'file://example.org/a'
+  );
 
-    urlJoin(input);
+  assert.equal(
+    urlJoin('file:/', 'example.org', 'a'),
+    'file://example.org/a'
+  );
 
-    input.should.eql(expected);
-  });
+  assert.equal(
+    urlJoin('file:', '/example.org', 'a'),
+    'file://example.org/a'
+  );
+
+  assert.equal(
+    urlJoin('file:', '//example.org', 'a'),
+    'file://example.org/a'
+  );
+});
+
+test('skips empty strings', () => {
+  assert.equal(
+    urlJoin('http://foobar.com', '', 'test'),
+    'http://foobar.com/test'
+  );
+
+  assert.equal(
+    urlJoin('', 'http://foobar.com', '', 'test'),
+    'http://foobar.com/test'
+  );
+});
+
+test('returns an empty string if no arguments are supplied', () => {
+  assert.equal(
+    urlJoin(),
+    ''
+  );
+});
+
+test('does not mutate the original input', () => {
+  const input = ['http:', 'www.google.com/', 'foo/bar', '?test=123'];
+  const expected = Array.from(input);
+
+  urlJoin(input);
+
+  assert.deepEqual(input, expected);
 });
